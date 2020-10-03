fcd0944399915e2b5ddc2ed0e5df16b2fcbb6a2b
Hamza Alalach <hamzaalalach@gmail.com>
2020-10-03 14:40:50 +0100

    Add Route

--- src/lib/Route.js
+++ src/lib/Route.js
@@ -1,19 +1,40 @@
 const DirectedGraph = require('graphology');
 const crypto = require('crypto');
 
-export const randomEdge = () => {
+const randomEdge = () => {
   return crypto.randomBytes(8).toString('hex');
 };
 
-export const Route = (start, finish, distance) => {
-  return new DirectedGraph();
+const Route = function(distance, start, finish) {
+  const route = new DirectedGraph();
+  this.start = start || randomEdge();
+  this.finish = finish || randomEdge();
+
+  route.addNode(this.start);
+  route.addNode(this.finish);
+  route.addDirectedEdge(this.start, this.finish);
+  route.setAttribute('distance', distance);
+
+  this.proto = route;
 };
 
-const graph = new DirectedGraph();
+Route.prototype.distance = function(distance) {
+  if (distance) {
+    this.proto.setAttribute('distance', distance);
+  } else {
+    return this.proto.getAttribute('distance');
+  }
+};
 
-graph.addNode('node1');
-graph.addNode('node2');
-graph.addDirectedEdge('node1', 'node2');
-graph.addDirectedEdge('node2', 'node1');
+Route.prototype.addRouteToStart = function(distance, start) {
+  const Start = start || randomEdge();
+  this.proto.addNode(Start);
+  this.proto.addDirectedEdge(Start, this.proto.nodes()[0]);
+  this.distance(distance);
+};
 
-console.log(graph.size);
+const route1 = new Route(500, 'A1', 'A2');
+route1.addRouteToStart(200);
+console.log(route1.proto.edges());
+
+module.exports = Route;
